168ef72726d9d35bfa456b3caa76b62f4bea983c
F-Holm <moshishafcdodgecharger@gmail.com>
2025-10-08 12:29:17 -0300

    Crear archivos

--- TP2V1_K1021G10_HOLM FEDERICO.cpp
+++ TP2V1_K1021G10_HOLM FEDERICO.cpp
@@ -27,6 +27,8 @@
 // Definiciones de Structs
 
 // Declaraciones de Funciones
+long GetDate(int &year, int &mes, int &dia, int &ds);
+long GetTime(int &hh, int &mm, int &ss);
 
 // Main
 
@@ -35,3 +37,29 @@ int main() {
 }
 
 // Definiciones de funciones
+
+long GetDate(int &year, int &mes, int &dia, int &ds) {
+  time_t rawtime;
+  struct tm *timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  year = 1900 + timeinfo->tm_year;
+  mes = 1 + timeinfo->tm_mon;
+  dia = timeinfo->tm_mday;
+  ds = 1 + timeinfo->tm_wday;
+  return (1900 + timeinfo->tm_year) * 10000 + (1 + timeinfo->tm_mon) * 100 +
+         timeinfo->tm_mday;
+}  // GetDate
+
+long GetTime(int &hh, int &mm, int &ss) {
+  time_t rawtime;
+  struct tm *timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  hh = timeinfo->tm_hour;
+  mm = timeinfo->tm_min;
+  ss = timeinfo->tm_sec;
+  return timeinfo->tm_hour * 10000 + timeinfo->tm_min * 100 + timeinfo->tm_sec;
+}  // GetTime
