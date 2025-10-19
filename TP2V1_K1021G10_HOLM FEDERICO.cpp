6589d0f409630c287b96812fba416ac5a6d96dcb
F-Holm <moshishafcdodgecharger@gmail.com>
2025-10-19 18:25:50 +0000

    funciona todo

--- TP2V1_K1021G10_HOLM FEDERICO.cpp
+++ TP2V1_K1021G10_HOLM FEDERICO.cpp
@@ -263,40 +263,41 @@ void ListVueAeropSld(ifstream &Aerops, ifstream &Vues, tvrAerop &vrAerop,
 
   for (ushort i = 0; i < CANT_AEROP; i++) {
     Aerops.clear();
-    Aerops.seekg(i * sizeof(sAerop));
-    Aerops.read((char *)&rAerop, sizeof(rAerop));
+    Aerops.seekg(vrAerop[i].pos * sizeof(sAerop));
+    Aerops.read((char *)&rAerop, sizeof(sAerop));
 
     cout << "\nAerop. origen: " << rAerop.codIATA << "  " << rAerop.nomAeropto
          << "  Ciudad: " << rAerop.ciudad
-         << "\nNroVue.   Empresa   Marca        Ciu.Dest.       "
-            "Nom.Aerop.Dest.   Estado   dia  hhAct  hhSa  t.V.  hhLl\n";
+         << "\nNroVue.   Empresa  Marca       Ciu.Dest.       "
+            " Nom.Aerop.Dest.  Estado          dia hhAct hhSa  t.V.  hhLl\n";
 
-    while (lVues && strncmp(lVues->info.nroVuelo, rAerop.codIATA, 3) == 0) {
+    while (lVues != NULL &&
+           strncmp(lVues->info.nroVuelo, rAerop.codIATA, 3) == 0) {
       Vues.clear();
-      Vues.seekg(lVues->info.pos * sizeof(rVue));
-      Vues.read((char *)&rVue, sizeof(rVue));
-
-      str3 codDest;
-      strncpy(codDest, rVue.nroVuelo + 6, 3);
-      codDest[3] = '\0';
+      Vues.seekg(lVues->info.pos * sizeof(sVue));
+      Vues.read((char *)&rVue, sizeof(sVue));
 
       Aerops.clear();
-      Aerops.seekg(vrAerop[BusBinVec(vrAerop, codDest)].pos * sizeof(sAerop));
+      Aerops.seekg(vrAerop[BusBinVec(vrAerop, rVue.nroVuelo + 6)].pos *
+                   sizeof(sAerop));
       Aerops.read((char *)&aeropDest, sizeof(sAerop));
 
-      short hhSa, mmSa, hhVi, mmVi, hhLl, mmLl,
-          diaSa = rVue.fechaSale - (rVue.fechaSale / 1000000 * 1000000);
+      short hhSa, mmSa, hhVi, mmVi, hhLl, mmLl, diaSa = rVue.fechaSale % 100;
       FormatoHoraMin(rVue.horaSale, hhSa, mmSa);
       HoraLlega(rVue.distKm, rVue.velCrucero, hhSa, mmSa, hhVi, mmVi, hhLl,
                 mmLl);
+
+      aeropDest.ciudad[16] = '\0';
+      aeropDest.nomAeropto[16] = '\0';
+      aeropDest.provin[15] = '\0';
 
       cout << setw(9) << rVue.nroVuelo << ' ' << setw(8) << rVue.empresa << ' '
            << setw(11) << rVue.marcaAeronv << ' ' << setw(16)
            << aeropDest.ciudad << ' ' << setw(16) << aeropDest.nomAeropto << ' '
            << setw(15) << aeropDest.provin << ' ' << setw(2) << diaSa << ' '
-           << setw(2) << setfill('0') << hh << ':' << setw(2) << mm << "   "
-           << setw(2) << hhSa << ':' << setw(2) << mmSa << "   " << setw(2)
-           << hhVi << ':' << setw(2) << mmVi << "   " << setw(2) << hhLl << ':'
+           << setfill('0') << setw(2) << hh << ':' << setw(2) << mm << ' '
+           << setw(2) << hhSa << ':' << setw(2) << mmSa << ' ' << setw(2)
+           << hhVi << ':' << setw(2) << mmVi << ' ' << setw(2) << hhLl << ':'
            << setw(2) << mmLl << setfill(' ') << '\n';
 
       SacarPrimerNodo(lVues);
